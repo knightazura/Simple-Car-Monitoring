18a6ada9cc6b5d8670b3ea6f65762b2bca26106a
Muhammad Izzuddin al Fikri <izzuddin.alfikri@icloud.com>
2017-11-28 05:39:08 +0700

    #32 Update report

--- resources/views/layouts/print-2.blade.php
+++ resources/views/layouts/print-2.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.print-wrapper')
 
 @section('content')
-<div class="container" style="border: 3px double #999">
+<div class="container" style="border: 1px double #999">
 
   <!-- 1st table -->
-  <div class="row justify-content-md-center pb-3" style="border-bottom: 1px dashed #999; font-size: 10pt">
+  <div class="row justify-content-md-center pb-2" style="border-bottom: 1px dashed #999; font-size: 10pt">
     <div class="col-md-12">
       <table>
-        <tr><td colspan="12">&nbsp;</td></tr>
         <tr>
           <td colspan="4" class="w-40">
             <b><u>PLN UIP SULBAGSEL</u></b>
@@ -28,36 +27,50 @@
           </td>
         </tr>
         <tr>
-          <td colspan="2" class="w-20"><small>Nama Pemohon</small></td>
+          <td colspan="2" class="w-25"><small>Nama Pemohon</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->requestedBy->employee_name }}</small></td>
         </tr>
         <tr>
-          <td colspan="2" class="w-20"><small>Jabatan</small></td>
+          <td colspan="2" class="w-25"><small>Jabatan</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->requestedBy->employee_position }}</small></td>
         </tr>
         <tr>
-          <td colspan="2" class="w-20"><small>Jenis Kendaraan</small></td>
+          <td colspan="2" class="w-25"><small>Jenis Kendaraan</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->car_plat_number }} ({{ $data->carStatus->theCar->car_name }})</small></td>
         </tr>
         <tr>
-          <td colspan="2" class="w-20"><small>Jumlah Penumpang</small></td>
+          <td colspan="2" class="w-25"><small>Jumlah Penumpang</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->total_passengers }} orang</small></td>
         </tr>
         <tr>
-          <td colspan="2" class="w-20"><small>Tempat Tujuan</small></td>
+          <td colspan="2" class="w-25"><small>Tempat Tujuan</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->destination }}</small></td>
         </tr>
         <tr>
-          <td colspan="2" class="w-20"><small>Maksud Perjalanan</small></td>
+          <td colspan="2" class="w-25"><small>Maksud Perjalanan</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->necessity }}</small></td>
         </tr>
         <tr>
-          <td colspan="2" class="w-20"><small>Tanggal yang diinginkan</small></td>
+          <td colspan="2" class="w-25"><small>Tanggal yang diinginkan</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ date('d-m-Y H:i:s', strtotime($data->desire_time)) }}</small></td>
         </tr>
         <tr>
-          <td colspan="2" class="w-20"><small>Pengambilan BBM</small></td>
+          <td colspan="2" class="w-25"><small>Pengambilan BBM</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->fuel_usage }} L</small></td>
+        </tr>
+        <tr>
+          <td colspan="2" class="w-25"><small>Estimasi waktu penggunaan</small></td>
+          <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->estimates_time }} hari</small></td>
+        </tr>
+        <tr>
+          <td colspan="2" class="w-25"><small>Keterangan</small></td>
+          <td colspan="10" class="text-left"><small>: 
+            @if(is_null($data->additional_description))
+              . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+            @else
+              {{ $data->additional_description }}
+            @endif
+          </small></td>
         </tr>
         <tr><td colspan="12">&nbsp;</td></tr>
         <tr>
@@ -85,7 +98,7 @@
   </div>
 
   <!-- 2nd table || Surat Penugasan -->
-  <div class="row justify-content-md-center mt-3" style="font-size: 10pt">
+  <div class="row justify-content-md-center mt-2" style="font-size: 10pt">
     <div class="col-md-12">
       <table>
         <tr>
@@ -101,7 +114,7 @@
             <small>Nomor: {{ $data->id }} / MUM.00.07 / UIPSULBAGSEL / {{date('Y')}}</small>
           </td>
         </tr>
-        <tr><td colspan="12">&nbsp;</td></tr>
+        <!-- <tr><td colspan="12">&nbsp;</td></tr> -->
         <tr>
           <td colspan="12">
             <small>Dengan ini menugaskan {{ $data->drivenBy->workOn->company_name }}
@@ -135,6 +148,10 @@
         <tr>
           <td colspan="2" class="w-20"><small>Tanggal yang diinginkan</small></td>
           <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ date('d-m-Y H:i:s', strtotime($data->desire_time)) }}</small></td>
+        </tr>
+        <tr>
+          <td colspan="2" class="w-25"><small>Estimasi waktu penggunaan</small></td>
+          <td colspan="10" style="border-bottom: 1px solid #999" class="text-left"><small>: {{ $data->estimates_time }} hari</small></td>
         </tr>
         <tr><td colspan="12">&nbsp;</td></tr>
         <tr>
